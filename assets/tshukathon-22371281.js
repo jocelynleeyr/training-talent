d3dfa0bbe0dbc3cbc8377581238b71fa2084969a
jocelynleeyr <kikilee7521@gmail.com>
2023-10-29 14:01:54 +0800

    deploy

---
+++ assets/tshukathon-22371281.js
@@ -0,0 +1,1 @@
+import{d as t}from"./index-f2802f18.js";const s=t("loading",{state:()=>({isLoading:!1})}),n="/training-talent/assets/tshukathon-877ff26f.jpg";export{n as t,s as u};
--- assets/tshukathon-8ca5a732.js
+++
@@ -1,1 +0,0 @@
-import{d as t}from"./index-27d8646d.js";const s=t("loading",{state:()=>({isLoading:!1})}),n="/training-talent/assets/tshukathon-877ff26f.jpg";export{n as t,s as u};
